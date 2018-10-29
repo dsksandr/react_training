856cbff350c2df62b496e77f2014fdc23900c3d6
Сергей Маюн <sergej.mayun@academmedia.com>
2018-10-29 12:19:54 +0700

    Добавил компонент news в компонент app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import News from './News/News';
+import Comments from './Comments/Comments';
+
+const myNews = [
+  {
+    id: '1',
+    author: 'Саша Печкин',
+    text: 'В четверг, четвертого числа...'
+  },
+  {
+    id: '2',
+    author: 'Просто Вася',
+    text: 'Считаю, что $ должен стоить 35 рублей!'
+  },
+  {
+    id: '3',
+    author: 'Max Frontend',
+    text: 'Прошло 2 года с прошлых учебников, а $ так и не стоит 35'
+  },
+  {
+    id: '4',
+    author: 'Гость',
+    text: 'Бесплатно. Без смс, про реакт, заходи - https://maxpfrontend.ru'
+  }
+];
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='container'>
+        <News data={myNews} />
+        <Comments />
       </div>
     );
   }
