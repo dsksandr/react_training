d887027d599f0f7d50329eec9627b442fea81581
Сергей Маюн <sergej.mayun@academmedia.com>
2018-10-29 13:36:18 +0700

    Добавил возможность отобразить полный текст новости, потестировал работу input

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,32 +5,65 @@ import Comments from './Comments/Comments';
 
 const myNews = [
   {
-    id: '1',
+    id: 1,
     author: 'Саша Печкин',
-    text: 'В четверг, четвертого числа...'
+    text: 'В четверг, четвертого числа...',
+    bigText: 'В четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили чёрными чернилами чертёж.'
   },
   {
-    id: '2',
+    id: 2,
     author: 'Просто Вася',
-    text: 'Считаю, что $ должен стоить 35 рублей!'
+    text: 'Считаю, что $ должен стоить 35 рублей!',
+    bigText: 'А евро 42!'
   },
   {
-    id: '3',
+    id: 3,
     author: 'Max Frontend',
-    text: 'Прошло 2 года с прошлых учебников, а $ так и не стоит 35'
+    text: 'Прошло 2 года с прошлых учебников, а $ так и не стоит 35',
+    bigText: 'А евро опять выше 70.'
   },
   {
-    id: '4',
+    id: 4,
     author: 'Гость',
-    text: 'Бесплатно. Без смс, про реакт, заходи - https://maxpfrontend.ru'
+    text: 'Бесплатно. Без смс, про реакт, заходи - https://maxpfrontend.ru',
+    bigText: 'Еще есть группа VK, telegram и канал на youtube! Вся инфа на сайте, не реклама!'
   }
 ];
+class TestInput extends Component{
+  constructor(props){
+    super(props);
+    this.input = React.createRef()
+  }
+  componentDidMount(){
+    this.input.current.focus();
+  }
+  onChangeHandler = event => {
+    this.setState({myValue: event.currentTarget.value})
+  };
+  showState = event => {
+    alert(this.input.current.value);
+  };
 
+  render() {
+    return (
+      <div>
+        <input
+          className='testInput'
+          onChange={this.onChangeHandler}
+          defaultValue=''
+          placeholder='Введите значение'
+          ref={this.input}/>
+        <button onClick={this.showState}>Отправить</button>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className='container'>
+        <TestInput />
         <News data={myNews} />
         <Comments />
       </div>
--- my-app/src/News/New/New.js
+++ my-app/src/News/New/New.js
@@ -3,12 +3,27 @@ import PropTypes from 'prop-types';
 import './newStyle.css';
 
 class New extends Component{
+  state = {
+    visible: false,
+  };
+  handleReadMoreClick = event => {
+    event.preventDefault();
+    this.setState({visible: true})
+  };
   render() {
-    const {author, text} = this.props.data
+    const {author, text, bigText} = this.props.data;
+    const {visible} = this.state;
+    console.log('render', this);
     return (
       <div className='new'>
         <p className='news__author'>{author}</p>
         <p className='news__text'>{text}</p>
+        {
+            !visible && <a onClick={this.handleReadMoreClick} href="#" className='news__readMore'>Подробнее</a>
+        }
+        {
+          visible && <p className='news__bigText'>{bigText}</p>
+        }
       </div>
     )
   }
@@ -17,7 +32,8 @@ class New extends Component{
 New.propTypes = {
   data: PropTypes.shape({
     author: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired
+    text: PropTypes.string.isRequired,
+    bigText: PropTypes.string.isRequired
   })
 };
 
