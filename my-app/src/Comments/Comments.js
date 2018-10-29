0c9b250d83c4949eb671fb0bc9140e6fac1167a0
Сергей Маюн <sergej.mayun@academmedia.com>
2018-10-29 12:21:08 +0700

    Создал комопонент comments

---
+++ my-app/src/Comments/Comments.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Comments extends Component{
+  render() {
+    return (
+      <div>
+        <p>Новостей нет - комментировать нечего!</p>
+      </div>
+    );
+  }
+}
+
+export default Comments;
